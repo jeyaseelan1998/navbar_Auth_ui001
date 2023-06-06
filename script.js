22f3e890e33372842ecb1706770c3438da9a12fe
jeyaseelan1998 <jeyaseelan.work@gmail.com>
2023-06-06 11:01:46 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let wrapper = document.querySelector('.wrapper');
+let registerLink = document.querySelector('.register-link');
+let loginLink = document.querySelector('.login-link');
+let btnLoginPopup = document.querySelector('.login-button-popup');
+let formClose = document.querySelector('.icon-close');
+let quoteEl = document.querySelector('.random-quote');
+
+
+registerLink.onclick = () => {
+    wrapper.classList.add('active');
+}
+
+loginLink.onclick = () => {
+    wrapper.classList.remove('active');
+}
+
+
+btnLoginPopup.onclick = () => {
+    wrapper.classList.add('active-popup');
+}
+
+formClose.onclick = () => {
+    wrapper.classList.remove('active-popup');
+}
+
+let url = "https://api.quotable.io/quotes/random?limit=1";
+
+fetch(url)
+.then(response => response.json())
+.then(result => {
+    quoteEl.innerHTML = result[0]['content'];
+})
