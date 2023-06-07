f0da51f1567f8ee1ecd30ceed19201fd8e11803e
jeyaseelan1998 <jeyaseelan.work@gmail.com>
2023-06-07 19:33:25 +0530

    minor changes updated

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let wrapper = document.querySelector('.wrapper');
+let navbar = document.querySelector('.nav-bar');
 let registerLink = document.querySelector('.register-link');
 let loginLink = document.querySelector('.login-link');
 let btnLoginPopup = document.querySelector('.login-button-popup');
 let formClose = document.querySelector('.icon-close');
 let quoteEl = document.querySelector('.random-quote');
 
+
+console.log(navbar.style);
 
 registerLink.onclick = () => {
     wrapper.classList.add('active');
@@ -23,10 +26,15 @@ formClose.onclick = () => {
     wrapper.classList.remove('active-popup');
 }
 
-let url = "https://api.quotable.io/quotes/random?limit=1";
+const getFact = () => {
+    quoteEl.style.display = "none";
+    let url = "https://api.quotable.io/quotes/random?limit=1";
+    fetch(url)
+    .then(response => response.json())
+    .then(result => {
+        quoteEl.style.display = "block";
+        quoteEl.innerHTML = result[0]['content'];
+    })
+}
 
-fetch(url)
-.then(response => response.json())
-.then(result => {
-    quoteEl.innerHTML = result[0]['content'];
-})
+getFact();
